5f8010d6c93beda1526aa2e1a0607c1817a1bc48
stefan <hodoreanu2004@gmail.com>
2026-05-21 15:56:49 +0300

    Added repositories for Member and Book

---
+++ src/main/java/com/stefanhodoreanu/libraryapi/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.stefanhodoreanu.libraryapi.repository;
+
+import com.stefanhodoreanu.libraryapi.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
